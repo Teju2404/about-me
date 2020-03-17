db5504b0bf0162d770b131462442e833b2dcd54b
Teju2404 <kandulatejaswireddy@gmail.com>
2020-03-17 17:50:46 -0500

    js changes made

--- main.js
+++ main.js
@@ -23,6 +23,10 @@
     let rectangle = (base, height) =>{
         return base*height;
     }
+
+    document.querySelector('#calculateRectangle').addEventListener('click', () => {
+        calculateRectangle();
+    });
     
     function calculateRectangle(){
         let baseValue = parseInt(document.getElementById("base").value);
@@ -30,5 +34,15 @@
         let result = rectangle(baseValue, heightValue);
         let resultElement = document.getElementById("resutSet");
         resultElement.innerHTML = "Area of Rectangle is : "+result;
+
+        if (baseValue != null) {
+            localStorage.setItem("baseValue", baseValue);
+        }
+        if (heightValue != null) {
+            localStorage.setItem("heightValue", heightValue);
+        }
+        if (result != null) {
+            localStorage.setItem("rectangleArea", result);
+        }
     }
 })()
